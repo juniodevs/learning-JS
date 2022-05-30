16b4ff7fb891a4a961819473c5af5b1972c38e49
Júnior Veras <iramarjunhorveras@gmail.com>
2022-05-30 08:17:28 -0300

    Adição de "Arrays.js, booleanos.js, objeto.js"

---
+++ Fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {};
+prod1.nome = 'Mega';
+prod1.preco = 4998.90;
+prod1['Desconto Teste'] = 0.50 //Evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = {
+    nome: 'Celular',
+    preco: 1799.90
+    };
+
+console.log(prod2);
