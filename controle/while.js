596eeb5db86749fca60694675f6460cd60fa1bee
Júnior Veras <iramarjunhorveras@gmail.com>
2022-06-10 09:59:04 -0300

    Adição "dowhile.js, for.js, switch.js, while.js"

---
+++ controle/while.js
@@ -0,0 +1,15 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0;
+
+do
+{
+    opcao = getInteiroAleatorioEntre(-1, 10);
+    console.log(`Opção escolhioda foi ${opcao}.`)
+}
+while (opcao != -1);
+
+console.log ('Até a Próxima');
