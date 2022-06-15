d3e6f4b0212ebf08dbd9a5426f9727a7fc427424
Júnior Veras <iramarjunhorveras@gmail.com>
2022-06-15 17:32:31 -0300

    Aula 2 (Variaveis e valores)

---
+++ Sintaxe Básica em JavaScript/Aula 2 - Variaveis/script.js
@@ -0,0 +1,38 @@
+// //tipos primitivos
+
+// //bolean
+// var vOuF = false;
+
+// console.log(vOuF);
+// console.log(typeof (vOuF));
+
+// //number
+// var numeroQualquer = 1;
+// console.log(numeroQualquer);
+// console.log(typeof(numeroQualquer));
+
+// //string
+// var nome = 'diana';
+// console.log(typeof (nome))
+// console.log(nome);
+
+//Como declarar
+
+// var variavel = 'diana'
+
+// variavel = 'martine'
+
+// console.log (variavel);
+
+// let variavel2 = 'diana';
+
+// console.log(variavel2);
+
+// variavel2 = 'martine';
+
+const constante = 'diana';
+
+// constante = 'martine';
+
+console.log (constante);
+
