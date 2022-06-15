abb6d5b5e2f4d5ed81abbc832d9d6bf3ac5acfd9
Júnior Veras <iramarjunhorveras@gmail.com>
2022-06-15 17:53:04 -0300

    Aula 2 (Variaveis)

--- Sintaxe Básica em JavaScript/Aula 2 - Variaveis/script.js
+++ Sintaxe Básica em JavaScript/Aula 2 - Variaveis/script.js
@@ -30,9 +30,102 @@
 
 // variavel2 = 'martine';
 
-const constante = 'diana';
+// const constante = 'diana';
 
-// constante = 'martine';
+// // constante = 'martine';
 
-console.log (constante);
+// console.log (constante);
 
+// var escopoGlobal = 'global';
+// console.log (escopoGlobal);
+
+// function escopoLocal(){
+//     let escopoLocalinterno = 'interno';
+//     console.log (escopoLocalinterno)
+// }
+
+// escopoLocal();
+
+//atribuição
+
+// var atribuicao = 'diana';
+
+// //Comparação
+
+// var comparacao = 0;
+
+// console.log (comparacao == '0');
+
+// //comparacao identica
+
+// var comparacaoIdentica = '0' === 0;
+// console.log(comparacaoIdentica);
+
+// //adicao
+
+// var adicao = 1 + 1;
+
+// console.log (adicao);
+
+// //subtracao
+
+// var subtracao = 2 - 1;
+
+// console.log (subtracao);
+
+// //multiplicacao
+
+// var multiplicacao = 2 * 3;
+
+// console.log (multiplicacao);
+
+// //divisao real
+
+// var divisaoreal = 6 / 2;
+
+// console.log (divisaoreal);
+
+// // divisao inteira
+
+// var divisaointeira = 5 % 2;
+// console.log (divisaointeira);
+
+// //potenciaçao
+
+// var potenciacao = 2 ** 10;
+
+// console.log(potenciacao);
+
+// //maior que
+
+// var maiorQue = 5 > 2;
+
+// console.log (maiorQue);
+
+// //menor que
+
+// var menorQue = 5 < 2;
+
+// console.log (menorQue);
+
+// //maior ou igual a
+
+// var maiorouiguala = 5 >= 2;
+
+// console.log (maiorouiguala);
+
+// //menor ou igual a
+
+// var maiorouiguala = 5 <= 2;
+
+// console.log (maiorouiguala);
+
+// var e = true && false;
+// console.log (e);
+
+// var ou = true || false;
+// console.log (ou);
+
+// var nao = !true;
+
+// console.log (nao);
