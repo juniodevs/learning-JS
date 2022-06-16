b1c3e12c7c714bc33e189bcdb84241f410b8e145
Júnior Veras <iramarjunhorveras@gmail.com>
2022-06-16 10:16:07 -0300

    Aula 5 (Funções e suas particularidades)

---
+++ Sintaxe Básica em JavaScript/Aula 5 - Funções e suas particularidades/script.js
@@ -0,0 +1,11 @@
+// function funcao(){
+//     console.log('Essa mensagem veio de uma funcao')
+// }
+
+// funcao();
+
+function mensagem (primeiro, segundo){
+    console.log (primeiro, segundo);
+}
+
+mensagem('Tudo Certo', 'Jovem');
