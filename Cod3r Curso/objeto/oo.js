ec5dcf1b805da9e7d642e028bb5bb977b39693ef
Júnior Veras <iramarjunhorveras@gmail.com>
2022-07-27 22:19:13 -0300

    Inicio de Estudo a OO

---
+++ Cod3r Curso/objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1,valor2,valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // FOco passou a ser o objeto
+
+// Principio Importantes
+// 1. abstracao
+// 2. encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
