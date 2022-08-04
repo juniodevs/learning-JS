8ac82f88ee94f32d88756e98b3fb9f2d2ff08324
Júnior Veras <iramarjunhorveras@gmail.com>
2022-08-04 11:50:19 -0300

    simulandoArray.js

---
+++ Cod3r Curso/array/simulandoArray.js
@@ -0,0 +1,13 @@
+const quaseArray = { 0: 'Rafael', 1: 'Ana', 2: 'Ana' }
+console.log(quaseArray)
+
+Object.defineProperty(quaseArray, 'toString', {
+    value: function() { return Object.values(this) },
+    enumerable: false
+})
+
+console.log(quaseArray[0])
+console.log(quaseArray[1])
+
+const meuArray = ['Rafael', 'Ana', 'Bia']
+console.log(quaseArray.toString(), meuArray)
